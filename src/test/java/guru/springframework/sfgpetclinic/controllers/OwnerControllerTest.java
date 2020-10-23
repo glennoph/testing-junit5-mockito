f0527c808119ac30ee38f58f496354716aa42a5e
glenn opdycke-hansen <glennoph@gmail.com>
2020-10-23 16:34:24 -0500

    add Mockito Argument Capture to OwnerControllerTest.java

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -1,18 +1,25 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
+import guru.springframework.sfgpetclinic.fauxspring.Model;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,6 +35,9 @@ class OwnerControllerTest {
     void setUp() {
         System.out.println("OwnerControllerTest");
     }
+
+
+
 
     @Test
     void processCreationForm_hasErrors() {
@@ -52,5 +62,30 @@ class OwnerControllerTest {
         verify(resultMock).hasErrors();
         verify(ownerServiceMock).save(any());
         verify(ownerMock).getId();
+    }
+
+    @Captor
+    ArgumentCaptor<String> argumentCaptor;
+    @Mock
+    BindingResult bindingResultMock;
+
+    @Test
+    void processFindFormWildcardArgCapture() {
+        //given
+        final String fname = "fname";
+        final String lname = "lname";
+        Owner owner = new Owner(11L, fname, lname );
+        List<Owner> ownerList = new ArrayList<>();
+        // capture argument of findAllByLastNameLike
+        given(ownerServiceMock.findAllByLastNameLike(argumentCaptor.capture()))
+                .willReturn(ownerList);
+        Model model = null;
+
+        //when
+        String viewRet = ownerControllerMock.processFindForm(owner, bindingResultMock, model);
+
+        //then
+        assertEquals("%"+lname+"%", argumentCaptor.getValue());
+
     }
 }
